3310c1868603956f71a7577af8650a4e83aef450
dj5822 <Dj5822@gmail.com>
2022-03-03 19:47:35 +1300

    Added homepage

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import TestButton from './components/TestButton';
+import Homepage from './pages/homepage';
 
-ReactDOM.render(<TestButton/>, document.getElementById("main-container"));
+ReactDOM.render(<Homepage/>, document.getElementById("main-container"));
---
+++ src/pages/homepage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import TestButton from '../components/TestButton';
+
+const Homepage = () => {
+    return (
+        <div>
+            <h1>React Test App</h1>
+            <TestButton />
+        </div>
+    );
+    
+}
+
+export default Homepage;
