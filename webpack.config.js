8069d39d832f27129288104030453b6cdb09c681
dj5822 <Dj5822@gmail.com>
2022-03-03 19:13:50 +1300

    Added eslint

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ESLintPlugin = require('eslint-webpack-plugin');
 
 module.exports = {
     // specify the entry point and output for the bundle.
@@ -27,6 +28,7 @@ module.exports = {
         new HtmlWebpackPlugin({
             template: path.join(__dirname, "src", "index.html"),
         }),
+        new ESLintPlugin(),
     ],
     mode: 'development'
 }
